a89d96881544738cb6983f9f004a4491abc61c67
ardhikaxx <ardhikayanuar58@gmail.com>
2025-09-21 05:55:12 +0700

    fix(manajemen-lokasi) penambahan sweet alert

--- resources/views/lokasi_jalur_lajur/index.blade.php
+++ resources/views/lokasi_jalur_lajur/index.blade.php
@@ -13,21 +13,6 @@
                     </a>
                 </div>
 
-                <!-- Hapus alert biasa -->
-                <!-- @if(session('success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        {{ session('success') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
-
-                @if(session('error'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ session('error') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif -->
-
                 <div class="table-responsive">
                     <table class="table table-bordered table-hover">
                         <thead class="table-light">
